abe9b19c5d8d1d394b9a9a1806f998729b148c60
Robson Pessoa <pessoa.robsonm@gmail.com>
2016-05-20 04:13:07 -0300

    Reorganised code.

--- Trabalho1/ManageRSA.java
+++ Trabalho1/ManageRSA.java
@@ -1,118 +1,99 @@
 import static java.lang.Math.sqrt;
-import java.util.Scanner;
+
 import java.util.List;
 import java.util.ArrayList;
 import java.math.*;
 
 public class ManageRSA {
+	private static final int CONSTANT_P = 257;
+	private static final int CONSTANT_Q = 263;
+	private static final int PRIVATE_KEY = 7;
 
-    private BigInteger n, z, p, q, pub, pvt;
+	private BigInteger n, z, p, q, publicKey, privateKey;
 
-    public ManageRSA() {
-        setVar(257, 263);
-        pub = autoPublic();
-        pvt = calcPvt(pub);
-        System.out.println("n: " + n);
-        System.out.println("z: " + z);
-        System.out.println("pub: " + pub);
-        System.out.println("pvt: " + pvt);
-    }
-    
-    public static void main(String args[]){
-        ManageRSA a = new ManageRSA();
-        
-        String message = "olar";
-        List<BigInteger> cyphered = a.CipherRSA(message);
-        
-        System.out.println(message 
-            + " - " + cyphered 
-            + " - " + a.unCipherRSA(cyphered));
-    }
-    
-    public List<BigInteger> CipherRSA(String message){
-        int i;
-        List<BigInteger> newMessage = new ArrayList<BigInteger>();
-        
-        for(i=0; i<message.length(); i++){
-            BigInteger c = BigInteger.valueOf(message.charAt(i));
-            System.out.println("Char: " + c.intValue());
-            newMessage.add(c.modPow(pub, n));
-            //newMessage.add((int)(Math.pow(c, pub) % n));
-        }
+	public ManageRSA() {
+		initVariables(CONSTANT_P, CONSTANT_Q);
+		publicKey = autoPublic();
+		privateKey = calculatePrivateKey(publicKey);
+	}
 
-        return newMessage;
-    }
-    
-    public String unCipherRSA(List<BigInteger> message){
-        int i;
-        String newMessage = new String();
-        
-        for(i=0; i<message.size(); i++){
-            BigInteger c = message.get(i);
-            BigInteger d = c.modPow(pvt, n);
-            System.out.println("New Char: " + d.intValue());
-            newMessage += Character.toString((char) d.intValue());
-        }
+	public List<BigInteger> encrypt(String message) {
+		int i;
+		List<BigInteger> newMessage = new ArrayList<BigInteger>();
 
-        return newMessage;
-    }
-    
-    public void setVar(int p, int q){
-        if(checkPrime(p)) this.p = BigInteger.valueOf(p);
-        if(checkPrime(q)) this.q = BigInteger.valueOf(q);
-        n = this.p.multiply(this.q);
-        z = this.p.subtract(BigInteger.valueOf(1)).multiply(this.q.subtract(BigInteger.valueOf(1)));
-    }
+		for (i = 0; i < message.length(); i++) {
+			BigInteger c = BigInteger.valueOf(message.charAt(i));
+			newMessage.add(c.modPow(publicKey, n));
+		}
 
-    public int setPub(BigInteger pub) {
-        if(checkPrime(pub.intValue()) && pub.compareTo(z) < 0) {
-            this.pub = pub;
-            pvt = calcPvt(pub);
-            
-            return 1;
-        }
-        
-        return -1;
-    }
+		return newMessage;
+	}
 
-    public BigInteger getN(){
-        return n;
-    }
-    
-    public BigInteger getPub() {
-        return pub;
-    }
+	public String decrypt(List<BigInteger> message) {
+		String newMessage = new String();
 
-    public BigInteger getPvt() {
-        return pvt;
-    }
+		for (int i = 0; i < message.size(); i++) {
+			BigInteger newChar = message.get(i).modPow(privateKey, n);
+			newMessage += Character.toString((char) newChar.intValue());
+		}
 
-    private BigInteger calcPvt(BigInteger pb) {
-        BigInteger pv = pub.modInverse(z);
-        
-        /*while(pb.multiply(pv).mod(z) != BigInteger.valueOf(1)){
-            pv = BigInteger.valueOf(getNextPrime(pv.intValue()));
-        }*/
-        
-        return pv;
-    }
-    
-    private BigInteger autoPublic(){
-        return BigInteger.valueOf(7);
-    }
+		return newMessage;
+	}
 
-    private int getNextPrime(int p){
-        while(!checkPrime(++p));
-        return p;
-    }
+	public void initVariables(int p, int q) {
+		if (checkPrime(p)) this.p = BigInteger.valueOf(p);
+		if (checkPrime(q)) this.q = BigInteger.valueOf(q);
+		n = this.p.multiply(this.q);
+		z = this.p.subtract(BigInteger.valueOf(1)).multiply(this.q.subtract(BigInteger.valueOf(1)));
+	}
 
-    private boolean checkPrime(int p) {
-        int i;
-        
-        for(i=2; i<sqrt(p)+1; i++){
-            if(p % i == 0) return false;
-        }
-        
-        return true;
-    }
+	public int setPublicKey(int pub) {
+		if (checkPrime(pub) && pub < z.intValue()) {
+			this.publicKey = BigInteger.valueOf(pub);
+			privateKey = calculatePrivateKey(BigInteger.valueOf(pub));
+			return 1;
+		}
+
+		return -1;
+	}
+
+	public BigInteger getN() {
+		return n;
+	}
+
+	public BigInteger getPublicKey() {
+		return publicKey;
+	}
+
+	public BigInteger getPrivateKey() {
+		return privateKey;
+	}
+
+	private BigInteger calculatePrivateKey(BigInteger pb) {
+		BigInteger pv = publicKey.modInverse(z);
+		return pv;
+	}
+
+	private BigInteger autoPublic() {
+		return BigInteger.valueOf(PRIVATE_KEY);
+	}
+
+	private boolean checkPrime(int p) {
+		for (int i = 2; i < sqrt(p) + 1; i++) {
+			if (p % i == 0) return false;
+		}
+
+		return true;
+	}
+
+	public static void main(String args[]) {
+		ManageRSA a = new ManageRSA();
+
+		String message = "olar";
+		List<BigInteger> cyphered = a.encrypt(message);
+
+		System.out.println(message
+				+ " - " + cyphered
+				+ " - " + a.decrypt(cyphered));
+	}
 }
