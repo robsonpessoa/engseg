42802dbf65019ec53c977f15490f3a63bc0de93b
Lucas Rovere <Lucas Rovere>
2016-05-21 17:46:16 -0300

    asdhashudhak

--- Trabalho1/SinCypher.java
+++ Trabalho1/SinCypher.java
@@ -2,18 +2,31 @@
 import java.math.*;
 
 public class SinCypher {
-	int keyA = 10, keyF = 10;
+	int key = 10;
 
 	public static void main(String args[]){
-		System.out.println("teste");
+		SinCypher sc = new SinCypher();
+		String message = "Goodbye World...";
+		String encrypted, decrypted;
+
+		encrypted = sc.encrypt(message);
+		decrypted = sc.decrypt(encrypted);
+
+		System.out.println("Original: " + message + "\nEncriptada: " + encrypted + "\nDecriptada: " + decrypted);
+
 	}
 
 	public String encrypt(String message) {
 		String newMessage = new String();
+		char temp[] = new char[message.length()];
 		int i;
 
-		for (i=0; i<message.length(); i++){
-			newMessage += (char)(((int) message.charAt(i)) + calculateKey(i));
+		for(i=0; i<message.length(); i++){
+			temp[(i*key)%message.length()] = message.charAt(i);
+		}
+
+		for(i=0; i<message.length(); i++){
+			newMessage += temp[i];
 		}
 
 		return newMessage;
@@ -21,16 +34,17 @@ public class SinCypher {
 
 	public String decrypt(String message){
 		String newMessage = new String();
+		char temp[] = new char[message.length()];
 		int i;
 
-		for (i=0; i<message.length(); i++){
-			newMessage += (char)(((int) message.charAt(i)) - calculateKey(i));
+		for(i=0; i<message.length(); i++){
+			temp[i] = message.charAt((i*key)%message.length());
+		}
+
+		for(i=0; i<message.length(); i++){
+			newMessage += temp[i];
 		}
 
 		return newMessage;
-	}
-
-	private int calculateKey(int position){
-		return (int) Math.sin(position*keyF)*keyA;
 	}
 }
