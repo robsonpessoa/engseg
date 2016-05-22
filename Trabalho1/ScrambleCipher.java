f2c3564cd14f02b7267668471809d3a401615ffe
Lucas Rovere <Lucas Rovere>
2016-05-21 23:49:20 -0300

    vai tomar no cu

--- Trabalho1/ScrambleCipher.java
+++ Trabalho1/ScrambleCipher.java
@@ -1,37 +1,47 @@
-public class ScrambleCipher {
-	private int publicKey = 7;
 
-	public void setPublicKey(int key) {
-		this.publicKey = 7;
+public class SinCypher {
+	int key = 13;
+
+	public static void main(String[] args){
+		SinCypher sc = new SinCypher();
+		String message = "Goodbye World...";
+		String encrypted, decrypted;
+
+		encrypted = sc.encrypt(message);
+		decrypted = sc.decrypt(encrypted);
+
+		System.out.println(message.length());
+		System.out.println("Original: " + message + "\nEncriptada: " + encrypted + "\nDecriptada: " + decrypted);
+
 	}
 
 	public String encrypt(String message) {
 		String newMessage = new String();
-		char temp[] = new char[message.length()];
+		int temp[] = new int[message.length()];
 		int i;
 
-		for (i = 0; i < message.length(); i++) {
-			temp[(i * publicKey) % message.length()] = message.charAt(i);
+		for(i=0; i<message.length(); i++){
+			temp[(i*key)%message.length()] = (int) (((int) message.charAt(i)) + Math.floor(7*Math.sin(i)));
 		}
 
-		for (i = 0; i < message.length(); i++) {
-			newMessage += temp[i];
+		for(i=0; i<message.length(); i++){
+			newMessage += (char) temp[i];
 		}
 
 		return newMessage;
 	}
 
-	public String decrypt(String message) {
+	public String decrypt(String message){
 		String newMessage = new String();
-		char temp[] = new char[message.length()];
+		int temp[] = new int[message.length()];
 		int i;
 
-		for (i = 0; i < message.length(); i++) {
-			temp[i] = message.charAt((i * publicKey) % message.length());
+		for(i=0; i<message.length(); i++){
+			temp[i] = (int) (message.charAt((i*key)%message.length()) - Math.floor(7*Math.sin(i)));
 		}
 
-		for (i = 0; i < message.length(); i++) {
-			newMessage += temp[i];
+		for(i=0; i<message.length(); i++){
+			newMessage += (char) temp[i];
 		}
 
 		return newMessage;
